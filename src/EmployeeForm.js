9ff56d535cf6c26b43ebee7a21758204010198c5
SambidMaharjan <sambid.maharjan@citytech.global>
2021-11-01 23:19:48 +0545

    implement image upload feature

--- src/EmployeeForm.js
+++ src/EmployeeForm.js
@@ -1,4 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import { Upload } from "antd";
+import ImgCrop from "antd-img-crop";
+
+import { Image } from "antd";
 import { Form, Input, Button } from "antd";
 import { Link } from "react-router-dom";
 
@@ -22,6 +26,27 @@ const EmployeeForm = () => {
 
     form.resetFields();
     localStorage.setItem("employeeRecord", JSON.stringify(employees));
+  };
+
+  const [fileList, setFileList] = useState([]);
+
+  const onChange = ({ fileList: newFileList }) => {
+    setFileList(newFileList);
+  };
+
+  const onPreview = async (file) => {
+    let src = file.url;
+    if (!src) {
+      src = await new Promise((resolve) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file.originFileObj);
+        reader.onload = () => resolve(reader.result);
+      });
+    }
+    const image = new Image();
+    image.src = src;
+    const imgWindow = window.open(src);
+    imgWindow.document.write(image.outerHTML);
   };
 
   return (
@@ -84,6 +109,18 @@ const EmployeeForm = () => {
           <Input />
         </Form.Item>
 
+        <ImgCrop rotate>
+          <Upload
+            action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+            listType="picture-card"
+            fileList={fileList}
+            onChange={onChange}
+            onPreview={onPreview}
+          >
+            {fileList.length < 5 && "+ Upload"}
+          </Upload>
+        </ImgCrop>
+
         <Button type="primary" htmlType="submit">
           Submit
         </Button>
@@ -94,5 +131,4 @@ const EmployeeForm = () => {
     </div>
   );
 };
-
 export default EmployeeForm;
